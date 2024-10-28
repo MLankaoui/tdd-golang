d41dc4a5af1779eb52de4357faf6989cb901f6c3
MLankaoui <marouanelankaoui66@gmail.com>
2024-10-28 19:16:30 +0100

    added new tests.

--- hello/hello.go
+++ hello/hello.go
@@ -2,8 +2,10 @@ package hello
 
 import "fmt"
 
+const englishPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name + "!"
+	return englishPrefix + name + "!"
 }
 
 func main() {
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,11 +3,27 @@ package hello
 import "testing"
 
 func TestXxx(t *testing.T) {
-	got := Hello("Marouane")
 
-	want := "Hello, Marouane!"
+	// testing the case where something is passed to our function
+	t.Run("when something is passed", func(t *testing.T) {
+		got := Hello("Marouane")
 
-	if got != want {
-		t.Errorf("got %q expected %q", got, want)
-	}
+		want := "Hello, Marouane!"
+
+		if got != want {
+			t.Errorf("got %q expected %q", got, want)
+		}
+	})
+
+	// testing the case when nothing is passed
+	t.Run("when nothing is passed", func(t *testing.T) {
+
+		got := Hello()
+
+		want := "Hello, world!"
+
+		if got != want {
+			t.Errorf("got %q expected %q", got, want)
+		}
+	})
 }
