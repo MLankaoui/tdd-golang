d88bea6bb77489603a89cc52d963699bfb98c232
MLankaoui <marouanelankaoui66@gmail.com>
2024-11-03 12:45:45 +0100

    add more tests.

---
+++ arrays_slices/sum.go
@@ -0,0 +1,10 @@
+package arraysslices
+
+func Sum(numbers []int) int {
+	result := 0
+
+	for _, number := range numbers {
+		result += number
+	}
+	return result
+}
--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -5,13 +5,27 @@ import "testing"
 func TestSum(t *testing.T) {
 	// creating and initializing an array
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("when we have a fixed size we use arrays", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 6}
 
-	got := Sum(numbers)
+		got := Sum(numbers)
 
-	want := 15
+		want := 16
 
-	if got != want {
-		t.Errorf("got '%d' want '%d', %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got '%d' want '%d', %v", got, want, numbers)
+		}
+	})
+
+	t.Run("when we don t have a fixed size we use slices", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+
+		want := 6
+
+		if got != want {
+			t.Errorf("got '%d' want '%d', %v", got, want, numbers)
+		}
+	})
 }
